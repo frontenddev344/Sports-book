3a2529014f2b963c96ab84f46e106bd26463bca2
devavology <145644105+devavology@users.noreply.github.com>
2024-04-19 18:49:21 +0530

    hfgj

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,25 +1,26 @@
-$(document).ready(function(){
-    $(".drop-down").click(function(){
+$(document).ready(function() {
+  $(".drop-down").click(function() {
       $(".sub-nav").addClass("menu-block");
-    });
-    $(".close-menu").click(function(){
+  });
+  $(".close-menu").click(function() {
       $(".sub-nav").removeClass("menu-block");
-    });
   });
-
-  $(document).ready(function(){
-  $(".toggle").click(function(){
-    $("header").addClass("menuToggle");
-  });
-  $(".close").click(function(){
-    $("header").removeClass("menuToggle");
-  });
-  });
-$(document).ready(function(){
- $(".mb-serch").click(function(){
-  $(".mobile-serch-bar").addClass("serch-block");
- });
- $(".serch-close").click(function(){
-  $(".mobile-serch-bar").removeClass("serch-block");
- });
 });
+
+$(document).ready(function() {
+  $(".toggle").click(function() {
+      $("header").addClass("menuToggle");
+  });
+  $(".close").click(function() {
+      $("header").removeClass("menuToggle");
+  });
+});
+$(document).ready(function() {
+  $(".mb-serch").click(function() {
+      $(".mobile-serch-bar").addClass("serch-block");
+  });
+  $(".serch-close").click(function() {
+      $(".mobile-serch-bar").removeClass("serch-block");
+  });
+});
+
